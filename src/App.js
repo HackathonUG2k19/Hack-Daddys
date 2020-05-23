f5b777483fd6bb5abf7d7203deb233772876fef3
Mehul1604 <mathurmehul3@gmail.com>
2020-05-23 11:44:06 +0530

    made a basic render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Helmet} from 'react-helmet'
+import {Helmet} from 'react-helmet';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'jquery';
+import 'popper.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>IIIT-H Auctioning App</title>
+        <style>{"body{background-color: #F08A00}"}</style>
+      </Helmet>
+      <p>Hello World</p>
+      
     </div>
   );
 }
