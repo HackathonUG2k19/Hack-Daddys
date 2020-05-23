a2eae83a0626b20be99f5bbb0e922901a4e32739
AakashJ2412 <aakashjain2412@gmail.com>
2020-05-24 00:09:12 +0530

    made sample navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Error from './components/Error';
-import Nav from './components/Nav';
+import Nav1 from './components/Nav';
  
 class App extends Component {
   render() {
     return (      
        <BrowserRouter>
         <div>
-          <Nav/>
+          <Nav1/>
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/about" component={About}/>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,16 +4,32 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Navbar,Nav} from 'react-bootstrap';
 import 'jquery';
 import 'popper.js';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const Nav = () => {
+const Nav1 = () => {
         return(
             <div>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/about">About</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
+         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand className="nav-logo">
+                <Link to={"/"}>
+                <img
+                    src={require("./img/auction_icon2.png")}
+                    width="60"
+                    className="d-inline-block align-top"
+                    alt="IIIT-H Auction logo" />{' '}
+                </Link>
+                    <div class="nav-text">IIIT-H <b><span className="nav-logo-gradient">Auction</span></b></div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto text-white">
+                        <Nav.Link><Link to={"/about"} className="text-white">About</Link></Nav.Link>
+                        <Nav.Link><Link to={"/contact"} className="text-white">Contact</Link></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
             </div>
         )
 }
-export default Nav;
+export default Nav1;
 
