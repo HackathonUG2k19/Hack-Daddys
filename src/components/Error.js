b2a055bf28f1890fd09481c60b9cd2a1ded9f884
Aakash-AA <aakash.aanegola@gmail.com>
2020-05-23 20:53:25 +0530

    Faulty Error.js Aakash please fix

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 const Error = () => {
-    return (
-       <div>
-          <p>Why are you here ?</p>
-       </div>
+    return ( < div >
+        <p> Why are you here ? < /p>  
+        <img src = { require("./img/daffaq.jpg")} alt = "aaaaa" />   
+        </div>
     );
 }
- 
+
 export default Error;
