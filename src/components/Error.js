846e26951cc50997aa0c3db587c669310b27b3f9
AakashJ2412 <aakashjain2412@gmail.com>
2020-05-23 23:49:41 +0530

    fixed aanegola error

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const Error = () => {
     return ( < div >
-        <p> Why are you here ? < /p>  
+        <p> Why are you here ? </p>  
         <img src = { require("./img/daffaq.jpg")} alt = "aaaaa" />   
         </div>
     );
